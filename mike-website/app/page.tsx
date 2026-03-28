e923424278a0e84f506c681db3e549ec5c86b1d9
Mike Wiegmink <mike.wiegmink@student.uva.nl>
2026-03-28 15:01:57 +0100

    Added introduction section to homepage

--- mike-website/app/page.tsx
+++ mike-website/app/page.tsx
@@ -1,13 +1,38 @@
-import Image from "next/image";
+import '@/styles/index.styles.css'
 
 export default function Home() {
 
+    const getAge = () => {
+    const date = new Date();
+    const now = {
+        day: date.getDate(),
+        month: date.getMonth()
+    };
+
+    if (now.month > 6 || (now.month === 6 && now.day >= 8)) {
+        return date.getFullYear() - 2005;
+    }
+
+    return date.getFullYear() - 1 - 2005;
+};
+
     return (
-        <div>
-            <div className="bodyContainer">
-                <p className="bodyText">This is the body</p>
-            </div>
-        </div>
+        <>
+        <section className="introContainer">
+            <section className="introTextContainer">
+                <h1>Hello! I'm Mike.</h1>
+                <p>I am a {getAge()} year old software developer from the Netherlands!</p>
+            </section>
+            <section className='introImageContainer'>
+                <img src={"Mike.jpeg"} className='imageMike'/>
+            </section>
+        </section>
+        <hr className='divider'/>
+        <section className="projectsContainer">
+            <p>Hier komen projecten</p>
+        </section>
+        <hr className='divider'/>
+       </>
     );
 
 }
