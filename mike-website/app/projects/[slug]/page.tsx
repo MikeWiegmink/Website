2ef96a03d37ba62bb78ca2222f8889215842dc79
Mike Wiegmink <mike.wiegmink@student.uva.nl>
2026-04-02 22:51:25 +0200

    Added basic projectpage template

--- mike-website/app/projects/[slug]/page.tsx
+++ mike-website/app/projects/[slug]/page.tsx
@@ -1,13 +1,35 @@
-export default async function ProjectDetails({ 
-    params 
-}: { 
-    params: Promise<{ slug: string }> 
-}) {
-  const { slug } = await params;
+import data from "@/constants/projects.json" with { type: 'json' };
+import "@/styles/projects.styles.css"
 
-  // const project = await getProjectBySlug(slug);
+export default async function ProjectDetails({ params }: { params: Promise<{ slug: string }>}) {
+    const { slug } = await params;
+    const project = data.filter((item) => item.slug == slug)[0]
 
-  return (
-    <p>{slug}</p>
-  );
+
+    return (
+        <section className="projectContainer">
+            <section className="titleContainer">
+                <h1>{project.title}</h1>
+                <p>Duration: {project.duration}</p>
+            </section>
+            <hr className="divider"/>
+            <section>
+                <p className="categoryTitle">Tech Stack</p>
+                <section className="skillsGrid">
+                    {project.stack?.map((item) => {
+                        return(
+                            <section className="skillBadge">
+                                <span>{item}</span>
+                            </section>
+                        )
+                    })}
+                </section>
+            </section>
+            <hr className="divider"/>
+            <section className="descriptionContainer">
+                <p className="categoryTitle">Description</p>
+                <p className="descriptionText">{project.description}</p>
+            </section>
+        </section>
+    );
 }
